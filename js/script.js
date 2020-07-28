9f5c6855fbd504635fe6039a5cdbab7a5e18788e
Gesha <geeeshaaa@gmail.com>
2020-07-28 13:02:33 +0300

    point 2

--- js/script.js
+++ js/script.js
@@ -7,13 +7,20 @@ const personalMovieDB = {
     genres: [],
     private:false
 };
-
+const personalDB = {
+    count:numberofFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private:false
+};
 const a = prompt('Один із останніх переглянутих фільмів?', ''),
       b = prompt('Як ви його оціните?',''),
       c = prompt('Один із останніх переглянутих фільмів?', ''),
       d = prompt('Як ви його оціните?','');
       personalMovieDB.movies[a] = b; 
       personalMovieDB.movies[c] = d;
+      
       
       console.log(personalMovieDB);
       
